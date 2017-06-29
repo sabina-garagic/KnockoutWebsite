2f0322a4548fea595ba6eccd284108123f6a4801
Sabina Garagic <sabina.garagic@check24.de>
2017-06-29 15:14:53 +0200

    Recipes from recipe-api displayed in table

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-// const $ = document.querySelector.bind(document);
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const userNameLabel = $('#user-name');
-//   const userNameInput = $('#user-name-input');
-//   const changeNameButton = $('#change-name-btn');
-
-//   function changeName() {
-//     userNameLabel.textContent = userNameInput.value;
-//   }
-
-//   changeNameButton.addEventListener('click', changeName);
-// });
 document.addEventListener('DOMContentLoaded', () => {
-  let ViewModel = function() {
-    this.userName = ko.observable("world"); 
-    this.userNameInput = ko.observable("");
+  const vm = {};  
+  vm.recipes = ko.observable();  
+  fetch('http://localhost:3000/api/recipes')
+    .then(result => result.json())
+    .then(recipes => vm.recipes(recipes));    
 
-    this.changeName = function() { 
-      this.userName(this.userNameInput());
-    }; 
-  }
-
-  ko.applyBindings(new ViewModel());
+  ko.applyBindings(vm);
 });
