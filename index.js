651439b1d5fb9215406d04fa7ee272e150499ebb
Sabina Garagic <sabina.garagic@check24.de>
2017-06-30 10:53:52 +0200

    Done

--- index.js
+++ index.js
@@ -1,9 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const vm = {};  
-  vm.recipes = ko.observable();  
-  fetch('http://localhost:3000/api/recipes')
-    .then(result => result.json())
-    .then(recipes => vm.recipes(recipes));    
+  const vm = {};
+  vm.recipes = ko.observable();
+  vm.recipeName = ko.observable();
+  vm.recipeInstructions = ko.observable();
+
+  const loadList = () => {
+    fetch('http://localhost:3000/api/recipes')
+      .then(result => result.json())
+      .then(recipes => vm.recipes(recipes));
+  }
+  loadList();
+
+  vm.addRecipe = () => {
+    fetch('http://localhost:3000/api/recipes',
+      {
+        method: "POST",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `name=${vm.recipeName()}&instructions=${vm.recipeInstructions()}`
+      })
+      .then(result =>
+        result.json())
+      .then(recipe => {
+        loadList();
+        console.log(recipe);
+      })
+  }
+
+  vm.deleteRecipe = recipe => {
+    fetch(`http://localhost:3000/api/recipes/${recipe._id}`,
+      {
+        method: "DELETE",
+      })
+      .then(result => result.json())
+      .then(recipe => {
+        loadList();
+        console.log(recipe);
+      })
+  }
+
+  vm.updateRecipe = (recipe) => {
+
+    fetch(`http://localhost:3000/api/recipes/${recipe._id}`,
+      {
+        method: "PUT",
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: `name=${recipe.name}&instructions=${recipe.instructions}`
+      })
+      .then(result => result.json())
+      .then(recipe => {
+        loadList();
+        console.log(recipe);
+      })
+
+  }
 
   ko.applyBindings(vm);
 });
